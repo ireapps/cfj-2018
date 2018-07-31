2a62889675b1aca9669669766e7ec20e4426571c
Cody Winchester <cjwinchester@gmail.com>
2018-07-31 15:12:01 -0600

    first up

---
+++ reference/myfile.py
@@ -0,0 +1,6 @@
+codys_dog = {
+    'name': 'Charlie',
+    'breed': '?',
+    'age_mos': 12,
+    'picture': 'https://github.com/cjwinchester/ire-2018-python-for-data-analysis/raw/master/img/charlie.jpg'
+}
